44f3647206dd91b998ccbb630de8fa60c036f9ae
yufu <kermit871229>
2021-11-02 10:29:33 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [aryData, setAryData] = useState([])
+
+  useEffect(() => {
+    function makeRequest() {
+      let url = "https://api.unsplash.com/photos/?client_id=812193ef71ca946e361ed541979a0cfd91e9419a19235fd05f51ea14233f020a&per_page=30";
+      let xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        // setAryData(this.response.split(/[\s\n]/))
+        console.log(JSON.parse(this.response))
+        let resultData=JSON.parse(this.response)
+        setAryData(resultData)
+      };
+      xhr.open("GET", url, true);
+      xhr.send();
+    }
+
+    makeRequest();
+  }, [])
+
+  function showData() {
+    return (
+      <div>
+        {
+          aryData.map((e, key) => (
+            <img src={e.urls.regular} alt=''/>
+          ))
+        }
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {aryData.length > 0 && showData()}
     </div>
   );
 }
